70409059aafdbe701a3339fa9372497eb246e7c6
A.Furkan Tanriverdi <ffurkan.tanriverdi@gmail.com>
2017-01-06 20:14:28 +0300

    pearson correlation coefficient calculation started

--- RecommenderSystem.py
+++ RecommenderSystem.py
@@ -1,5 +1,8 @@
+import math as math
+
 import numpy as np
 import pandas
+import scipy.stats.stats as pearsonr
 
 columnNames = ['movieID', 'userID', 'rating']
 
@@ -11,29 +14,74 @@ listOfMovieIDs = data.movieID.tolist()
 listOfUserIDs = data.userID.tolist()
 
 # unique movie IDs
-uniqueListOfMovieIds = pandas.unique(
-	listOfMovieIDs)  # pandas.unique is faster then numpy.unique and return_index= true by default
+# pandas.unique is faster then numpy.unique and return_index= true by default
+uniqueListOfMovieIds = pandas.unique(listOfMovieIDs)
 numberOfMovies = uniqueListOfMovieIds.size
 tempMovieEnum = dict(enumerate(uniqueListOfMovieIds))
-movieEnum = {v: k for k, v in tempMovieEnum.items()}
+movieEnum = {v: k for k, v in
+             tempMovieEnum.items()}  # inverse tempMovieEnum because enumerate does not return ordered way
 
 # unique user IDs
 uniqueListOfUserIDs = pandas.unique(listOfUserIDs)
 numberOfUsers = uniqueListOfUserIDs.size
 tempUserEnum = dict(enumerate(uniqueListOfUserIDs))
-userEnum = {v: k for k, v in tempUserEnum.items()}
+userEnum = {v: k for k, v in tempUserEnum.items()}  #inverse tempUserEnum because enumerate does not return ordered way
 
 # movie X user matrix
-userMovieRatingMatrix = np.empty(shape=(numberOfMovies, numberOfUsers), dtype=np.float)
+movieUserRatingMatrix = np.empty(shape=(numberOfMovies, numberOfUsers), dtype=np.float)
 
 
 ############################################## FUNCTIONS DEFINITIONS #############################################
+
+# First value is the r-value, 2nd is the p-value
+def scipyPearsonrTest( index1, index2 ):
+	print(pearsonr.pearsonr(movieUserRatingMatrix[:, index1], movieUserRatingMatrix[:, index2]))
+	return
 
 def buildMovieratingMatrix( data ):
 	for index, row in data.iterrows():
 		movieIndex = movieEnum[row['movieID']]
 		userIndex = userEnum[row['userID']]
-		userMovieRatingMatrix[movieIndex][userIndex] = row['rating']
+		movieUserRatingMatrix[movieIndex][userIndex] = row['rating']
+
+	return
+
+
+def getMeanRatingByUserIndex( activeUserIndex ):
+	sum = 0
+	numberOfRating = 0
+	for rating in movieUserRatingMatrix[:, activeUserIndex]:
+		if rating != 0:  # izlemediği filmleri hesaba katmıyoruz
+			numberOfRating += 1
+			sum += rating
+
+	return sum / numberOfRating
+
+
+def calculatePearsonCorrelation( activeUserIndex, userIndex ):
+	numerator = 0
+	denumeratorForActiveUser = 0
+	denumeratorForUser = 0
+	denumerator = 0
+
+	meanRatingGivenByActiveUser = getMeanRatingByUserIndex(activeUserIndex)
+	meanRatingGivenByUser = getMeanRatingByUserIndex(userIndex)
+
+	for movie in movieUserRatingMatrix:
+		ratingGivenByActiveUser = movie[activeUserIndex]
+		ratingGivenByUser = movie[userIndex]
+
+		numerator += (ratingGivenByActiveUser - meanRatingGivenByActiveUser) * (
+		ratingGivenByUser - meanRatingGivenByUser)
+		denumeratorForActiveUser = + math.pow(ratingGivenByActiveUser - meanRatingGivenByActiveUser, 2)
+		denumeratorForUser = + math.pow(ratingGivenByUser - meanRatingGivenByUser, 2)
+
+	denumerator = (math.sqrt(denumeratorForActiveUser)) * (math.sqrt(denumeratorForUser))
+
+	# r = pearson correlation
+	r = numerator / denumerator
+	print(r)
+	scipyPearsonrTest(activeUserIndex, userIndex)
 
 	return
 
@@ -42,3 +90,5 @@ def buildMovieratingMatrix( data ):
 
 # kullanıcılar tarafından herbir filme verilen puanlar tutuluyor
 buildMovieratingMatrix(data)
+
+calculatePearsonCorrelation(0,1)
